fa4b0cfb34a679b81665cc884b1af2f89ec6323f
nisargptl <nisarg3000@gmail.com>
2022-10-09 23:21:43 +0530

    Added SFINAE checks in JsonSerializer to ensure classes have the required SetJsonMapping method.

--- include/json_serializer.h
+++ include/json_serializer.h
@@ -11,9 +11,23 @@ enum class AutoJsonMethod {
     Unmarshal
 };
 
+// SFINAE to check if a class has a SetJsonMapping method
+template <typename T>
+class has_SetJsonMapping {
+private:
+    template <typename U>
+    static auto test(U*) -> decltype(std::declval<U>().SetJsonMapping(), std::true_type());
+
+    template <typename>
+    static std::false_type test(...);
+
+public:
+    static constexpr bool value = std::is_same<decltype(test<T>(nullptr)), std::true_type>::value;
+};
+
 class JsonSerializer {
 public:
-    template <typename T>
+    template <typename T, typename std::enable_if<has_SetJsonMapping<T>::value, int>::type = 0>
     static std::string Serialize(T& obj) {
         Json::Value root;
         obj.SetMethod(AutoJsonMethod::Marshal);
@@ -21,7 +35,7 @@ public:
         return obj.GetString();
     }
 
-    template <typename T>
+    template <typename T, typename std::enable_if<has_SetJsonMapping<T>::value, int>::type = 0>
     static void Deserialize(const std::string& json_string, T& obj) {
         Json::Value root;
         Json::CharReaderBuilder reader;
