7ea193f4cbe6b390a3cf4aa150513c1e9c0b1b81
nisargptl <nisarg3000@gmail.com>
2022-10-17 21:11:54 +0530

    Introduced private namespace for encapsulation.

--- include/json_serializer.h
+++ include/json_serializer.h
@@ -3,52 +3,58 @@
 
 #include <string>
 #include <type_traits>
-#include <json/json.h>
+#include "json/json.h"
 
-enum class AutoJsonMethod {
-    Default,
-    Marshal,
-    Unmarshal
-};
+namespace CppAutoSerializer {
+    namespace Internal {
 
-// SFINAE to check if a class has a SetJsonMapping method
-template <typename T>
-class has_SetJsonMapping {
-private:
-    template <typename U>
-    static auto test(U*) -> decltype(std::declval<U>().SetJsonMapping(), std::true_type());
+        enum class AutoJsonMethod {
+            Default,
+            Marshal,
+            Unmarshal
+        };
 
-    template <typename>
-    static std::false_type test(...);
+        // SFINAE to check if a class has a SetJsonMapping method
+        template <typename T>
+        class has_SetJsonMapping {
+        private:
+            template <typename U>
+            static auto test(U*) -> decltype(std::declval<U>().SetJsonMapping(), std::true_type());
 
-public:
-    static constexpr bool value = std::is_same<decltype(test<T>(nullptr)), std::true_type>::value;
-};
+            template <typename>
+            static std::false_type test(...);
 
-class JsonSerializer {
-public:
-    template <typename T, typename std::enable_if<has_SetJsonMapping<T>::value, int>::type = 0>
-    static std::string Serialize(T& obj) {
-        Json::Value root;
-        obj.SetMethod(AutoJsonMethod::Marshal);
-        obj.SetJsonMapping();
-        return obj.GetString();
-    }
+        public:
+            static constexpr bool value = std::is_same<decltype(test<T>(nullptr)), std::true_type>::value;
+        };
 
-    template <typename T, typename std::enable_if<has_SetJsonMapping<T>::value, int>::type = 0>
-    static void Deserialize(const std::string& json_string, T& obj) {
-        Json::Value root;
-        Json::CharReaderBuilder reader;
-        std::istringstream s(json_string);
-        std::string errs;
-        if (Json::parseFromStream(reader, s, &root, &errs)) {
-            obj.SetMethod(AutoJsonMethod::Unmarshal);
+    } // namespace Internal
+
+    class JsonSerializer {
+    public:
+        template <typename T, typename std::enable_if<Internal::has_SetJsonMapping<T>::value, int>::type = 0>
+        static std::string Serialize(T& obj) {
+            obj.SetMethod(Internal::AutoJsonMethod::Marshal);
             obj.SetJsonMapping();
-            obj._unmarshal_internal(root);
-        } else {
-            throw std::runtime_error("Failed to parse JSON: " + errs);
+            return obj.GetString();
         }
-    }
-};
+
+        template <typename T, typename std::enable_if<Internal::has_SetJsonMapping<T>::value, int>::type = 0>
+        static void Deserialize(const std::string& json_string, T& obj) {
+            Json::Value root;
+            Json::CharReaderBuilder reader;
+            std::istringstream s(json_string);
+            std::string errs;
+            if (Json::parseFromStream(reader, s, &root, &errs)) {
+                obj.SetMethod(Internal::AutoJsonMethod::Unmarshal);
+                obj.SetJsonMapping();
+                obj._unmarshal_internal(root);
+            } else {
+                throw std::runtime_error("Failed to parse JSON: " + errs);
+            }
+        }
+    };
+
+} // namespace CppAutoSerializer
 
 #endif // JSON_SERIALIZER_H
